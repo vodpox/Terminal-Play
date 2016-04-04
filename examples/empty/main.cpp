575b4f79d24630aacc36975e79092d3465dce331
Edvinas <edvinas.senda@gmail.com>
2016-04-04 22:26:20 +0300

    Updated example project to work with engine's graphics

--- examples/empty/main.cpp
+++ examples/empty/main.cpp
@@ -3,12 +3,41 @@
 #include "Input.h"
 
 class TScene : public Scene {
-public:
-  Game *game;
-  TScene(Game game) : game(&game) {}
-  void update(){
-    if(game->input.getButton(Keyboard::a)) printf("\033[2;31mHello World\033[0m\n");
-  }
+	public:
+		bool doDraw = true;
+		int x = 5;
+		int y = 5;
+	  Game *game;
+	  TScene(Game game) : game(&game) {}
+	  void update(){
+	    if(game->input.getButton(Keyboard::upArrow)){
+				y--;
+				doDraw = true;
+			}
+			else if(game->input.getButton(Keyboard::downArrow)){
+				y++;
+				doDraw = true;
+			}
+			else if(game->input.getButton(Keyboard::leftArrow)){
+				x--;
+				doDraw = true;
+			}
+			else if(game->input.getButton(Keyboard::rightArrow)){
+				x++;
+				doDraw = true;
+			}
+			else if(game->input.getButton(Keyboard::a)){
+				doDraw = true;
+			}
+	  }
+		
+		void draw(){
+	    if(doDraw){
+				game->graphics.addToScreen(x, y, "Hello, world!");
+				game->graphics.draw();
+				doDraw = false;
+			}
+	  }
 };
 
 int main(){
