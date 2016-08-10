75f23a9fc4ecc8af82637349412862115222a20b
edve98 <edvinas.senda@gmail.com>
2016-08-11 00:43:17 +0300

    Changed scene's name

--- examples/simple demo/main.cpp
+++ examples/simple demo/main.cpp
@@ -7,15 +7,15 @@
 #include "Graphics.h"
 
 
-class TestScene : public tplay::Scene {
+class ExampleScene : public tplay::Scene {
 	public:
 		tplay::Game *game;
-		TestScene(tplay::Game &game) : game(&game) {}
-		
+		ExampleScene(tplay::Game &game) : game(&game) {}
+
 		// Player's coordinates
 		int x = 5;
 		int y = 5;
-		
+
 		void update() {
 			// Get input. Arrow keys to move up and down, q quits game.
 			if (game->input.isButtonDown(tplay::Keyboard::UP_ARROW)) {
@@ -34,11 +34,11 @@ class TestScene : public tplay::Scene {
 				game->quit();
 			}
 		}
-		
+
 		void draw() {
 			// Centre camera on player
 			game->graphics.setCameraCoordinates(x - (game->graphics.getTerminalSizeX() / 2), y - (game->graphics.getTerminalSizeY() / 2));
-			
+
 			// Draw axis from (0, 0)
 			// X axis
 			game->graphics.setFormat(tplay::Format::FOREGROUND_BLUE);
@@ -50,7 +50,7 @@ class TestScene : public tplay::Scene {
 			game->graphics.setFormat(tplay::Format::BOLD);
 			game->graphics.addToScreen(40, 0, ">");
 			game->graphics.resetFormat();
-			
+
 			// Y axis
 			game->graphics.setFormat(tplay::Format::FOREGROUND_RED);
 			for (int i = -19; i < 20; i++) {
@@ -61,12 +61,12 @@ class TestScene : public tplay::Scene {
 			game->graphics.setFormat(tplay::Format::BOLD);
 			game->graphics.addToScreen(0, 20, "^");
 			game->graphics.resetFormat();
-			
+
 			// Middle point
 			game->graphics.setFormat(tplay::Format::BACKGROUND_GREEN);
 			game->graphics.addToScreen(0, 0, " ");
 			game->graphics.resetFormat();
-			
+
 			// Player
 			game->graphics.addToScreen(x, y, "@");
 		}
@@ -74,7 +74,7 @@ class TestScene : public tplay::Scene {
 
 int main() {
 	tplay::Game game;
-	TestScene scene(game);
+	ExampleScene scene(game);
 	game.setScene(&scene);
 	game.loop();
 	return 0;
